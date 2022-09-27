3231510a7c4eb132ca66c14014b82fcb3399dde0
dlgudwn55 <dkrlekeheoek@naver.com>
2022-09-27 13:57:05 +0900

    20220927-1
    search.py에서 솔랭과 자랭 정보 분리

--- main.py
+++ main.py
@@ -19,9 +19,20 @@ def search():
     if entry == None:
         return render_template('search.html', user_name=user_name, tier="Unranked", rank="")
     else:
-        tier, rank, wins, losses = extract_entries(summoner_id)
-        tier = tier.title()
-        return render_template('search.html', user_name=user_name, tier=tier, rank=rank)
+        info = extract_entries(summoner_id)
+        if info[0] == "Solo-only":
+            solo_tier, solo_rank, solo_wins, solo_losses = info[1:]
+            return render_template('search.html', user_name=user_name, solo_tier=solo_tier.title(), solo_rank=solo_rank,
+                solo_wins=solo_wins, solo_losses=solo_losses)
+        if info[0] == "Team-only":
+            team_tier, team_rank, team_wins, team_losses = info[1:]
+            return render_template('search.html', user_name=user_name, team_tier=team_tier.title(), team_rank=team_rank,
+                team_wins=team_wins, team_losses=team_losses)
+        if info[0] == "Both":
+            solo_tier, solo_rank, solo_wins, solo_losses, team_tier, team_rank, team_wins, team_losses = info[1:]
+            return render_template('search.html', user_name=user_name, solo_tier=solo_tier.title(), solo_rank=solo_rank,
+                solo_wins=solo_wins, solo_losses=solo_losses, team_tier=team_tier.title(), team_rank=team_rank,
+                team_wins=team_wins, team_losses=team_losses)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000)
--- search.py
+++ search.py
@@ -12,6 +12,13 @@ def extract_entries(summoner_id):
     if response.json() == []:
         return None
     response_json = response.json()
+    
     if len(response_json) == 1:
         if response_json[0]['queueType'] == 'RANKED_SOLO_5x5':
-            return response_json[0]['tier'], response_json[0]['rank'], response_json[0]['wins'], response_json[0]['losses']
+            return ["Solo-only", response_json[0]['tier'], response_json[0]['rank'], response_json[0]['wins'], response_json[0]['losses']]
+        if response_json[0]['queueType'] == 'RANKED_TEAM_5x5':
+            return ["Team-only", response_json[0]['tier'], response_json[0]['rank'], response_json[0]['wins'], response_json[0]['losses']]
+    
+    if len(response_json) == 2:
+        ["Both", response_json[0]['tier'], response_json[0]['rank'], response_json[0]['wins'], response_json[0]['losses'],
+        response_json[1]['tier'], response_json[1]['rank'], response_json[1]['wins'], response_json[1]['losses']]
