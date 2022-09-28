3cc93b2d6f28b1f9334a2f6e9e65b441194f4f92
dlgudwn55 <dkrlekeheoek@naver.com>
2022-09-28 13:49:06 +0900

    20220928-1
    added match history

--- main.py
+++ main.py
@@ -16,23 +16,48 @@ def search():
         return redirect('/')
     summoner_id = extract_id(user_name)
     entry = extract_entries(summoner_id)
+    matche_ids = extract_matches(user_name)
+    matches = []
+    for match_id in matche_ids:
+        matches.append(extract_match_info(match_id))
+        
+    my_idxs = []
+    for match in matches:
+        for participant in match["info"]["participants"]:
+            if participant["summonerName"] == user_name:
+                my_idxs.append(participant["participantId"]-1)
+    
+    match_data = []
+    for match in matches:
+        match_data.append({"mode": match["info"]["gameMode"], "gameDuration": match["info"]["gameDuration"]})
+    for i in range(len(my_idxs)):
+        match_data[i]["champion"] = matches[i]["info"]["participants"][my_idxs[i]]["championName"]
+        match_data[i]["kills"] = matches[i]["info"]["participants"][my_idxs[i]]["kills"]
+        match_data[i]["deaths"] = matches[i]["info"]["participants"][my_idxs[i]]["deaths"]
+        match_data[i]["assists"] = matches[i]["info"]["participants"][my_idxs[i]]["assists"]
+        # match_data[i]["win"] = matches[i]["info"]["participants"][my_idxs[i]]["win"]
+        if matches[i]["info"]["participants"][my_idxs[i]]["win"]:
+            match_data[i]["wl"] = "Win"
+        else:
+            match_data[i]["wl"] = "Loss"
+        
     if entry == None:
-        return render_template('search.html', user_name=user_name, tier="Unranked", rank="")
+        return render_template('search.html', user_name=user_name, tier="Unranked", rank="", match_data=match_data)
     else:
         info = extract_entries(summoner_id)
         if info[0] == "Solo-only":
             solo_tier, solo_rank, solo_wins, solo_losses = info[1:]
             return render_template('search.html', user_name=user_name, solo_tier=solo_tier.title(), solo_rank=solo_rank,
-                solo_wins=solo_wins, solo_losses=solo_losses)
+                solo_wins=solo_wins, solo_losses=solo_losses, match_data=match_data)
         if info[0] == "Team-only":
             team_tier, team_rank, team_wins, team_losses = info[1:]
             return render_template('search.html', user_name=user_name, team_tier=team_tier.title(), team_rank=team_rank,
-                team_wins=team_wins, team_losses=team_losses)
+                team_wins=team_wins, team_losses=team_losses, match_data=match_data)
         if info[0] == "Both":
             solo_tier, solo_rank, solo_wins, solo_losses, team_tier, team_rank, team_wins, team_losses = info[1:]
             return render_template('search.html', user_name=user_name, solo_tier=solo_tier.title(), solo_rank=solo_rank,
                 solo_wins=solo_wins, solo_losses=solo_losses, team_tier=team_tier.title(), team_rank=team_rank,
-                team_wins=team_wins, team_losses=team_losses)
-
+                team_wins=team_wins, team_losses=team_losses, match_data=match_data)
+            
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000)
--- search.py
+++ search.py
@@ -1,10 +1,15 @@
 from requests import get
-API_KEY = 'RGAPI-9870331e-9b22-4188-aa41-da34ca274d37'
+API_KEY = 'RGAPI-91bef776-6856-4cc5-be57-49c03ef3fb3f'
 
 def extract_id(summoner_name):
     base_url = 'https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/'
     response = get(f"{base_url}{summoner_name}", headers={"X-Riot-Token": API_KEY})
     return response.json()["id"]
+
+def extract_puuid(summoner_name):
+    base_url = 'https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/'
+    response = get(f"{base_url}{summoner_name}", headers={"X-Riot-Token": API_KEY})
+    return response.json()["puuid"]
 
 def extract_entries(summoner_id):
     base_url = 'https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/'
@@ -22,3 +27,14 @@ def extract_entries(summoner_id):
     if len(response_json) == 2:
         ["Both", response_json[0]['tier'], response_json[0]['rank'], response_json[0]['wins'], response_json[0]['losses'],
         response_json[1]['tier'], response_json[1]['rank'], response_json[1]['wins'], response_json[1]['losses']]
+        
+def extract_matches(summoner_name):
+    puuid = extract_puuid(summoner_name)
+    base_url = "https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/"
+    response = get(f"{base_url}{puuid}/ids?start=0&count=20", headers={"X-Riot-Token": API_KEY})
+    return response.json()
+    
+def extract_match_info(match_id):
+    base_url = "https://asia.api.riotgames.com/lol/match/v5/matches/"
+    response = get(f"{base_url}{match_id}", headers={"X-Riot-Token": API_KEY})
+    return response.json()
